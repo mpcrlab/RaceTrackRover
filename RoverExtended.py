fb524a3a2b7f5aa1cbe5787846071a1782cf2750
shawnmartin2013 <shawnmartin2013@fau.edu>
2017-01-27 13:54:30 -0500

    fixed reverse

--- RoverExtended.py
+++ RoverExtended.py
@@ -240,20 +240,22 @@ class RoverExtended(Rover):
             # Getting new treads based on angle
             self.getNewTreads()
 
-            # Ignore this, needed for fast tread switching
-            # and to not back up the tread switching queue
-            oldTreads = self.checkTreadStatus(oldTreads)
-
             # Boolean user-inputted controls
             if self.isReversed:
                 rev = self.treads[::-1]
-                self.treads = [rev[0] * -1, rev[1] * -1]
+                self.treads = [rev[0]* -1, rev[1] * -1]
             if self.paused:
                 self.freeze()
             if self.lightsOn:
                 self.turn_the_lights_on()
             else:
                 self.turn_the_lights_off()
+
+
+            # Ignore this, needed for fast tread switching
+            # and to not back up the tread switching queue
+            oldTreads = self.checkTreadStatus(oldTreads)
+
 
             # Saving data, currently not saving when rover is in reversed state
             self.isLearning = self.canSave and not self.isReversed and not self.paused
